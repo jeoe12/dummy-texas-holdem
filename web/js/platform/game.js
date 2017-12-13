1bbb119d9520f424f39b24d31e9c512f9ef0ef55
strawmanbobi <strawmanbobi@irext.net>
2017-12-13 21:32:48 +0800

    try using dynamic ws port

--- web/js/platform/game.js
+++ web/js/platform/game.js
@@ -117,8 +117,11 @@ $(document).ready(function () {
 function initWebsock() {
     // initialize web communication
     var host = window.location.hostname;
-    var port = '8080';
-    var serverAddress = 'ws://' + host + ':' + port;
+    var port = '80';
+    if (host === 'localhost') {
+        port = '8080';
+    }
+    var serverAddress = 'ws://' + host + ':' + port + "/game/";
 
     // TODO: to pickup a idle server from cluster
     console.log('guest connect to server, playerName = ' + playerNamePlain + ', tableNumber = ' + tableNumber);
